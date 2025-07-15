76c225001725001df61c5ed7141d10a98819aa1b
simransikha <132929457+simransikha@users.noreply.github.com>
2025-07-15 17:56:11 +0530

    last change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update if hosted elsewhere
+  baseURL: 'http://localhost:5000/api', 
+
+
+
   headers: {
     'Content-Type': 'application/json'
   }
